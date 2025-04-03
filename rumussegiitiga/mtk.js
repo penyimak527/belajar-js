8613b18a88f4af9eb637c08ee34858426754dfbe
penyimak527 <143236724+penyimak527@users.noreply.github.com>
2025-04-03 21:20:36 +0700

    rumus segitiga
    ini adalah contoh website menggunakan javascript untuk menggunakan rumus luas segitiga dasar

---
+++ rumussegiitiga/mtk.js
@@ -0,0 +1,10 @@
+alert('halo ini adalah website untuk menghitung rumus segitiga');
+
+var alas = prompt('ukuran alas : ');
+var tinggi = prompt('ukuran tinggi segitiga :')
+let rumus = 1/2;
+
+// operator
+var akhir = rumus * alas * tinggi;
+
+alert(`hasil luas segitiga adalah ${akhir} m2` );
